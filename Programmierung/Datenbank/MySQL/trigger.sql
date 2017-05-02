df1eafb063d9687d99ef91afe1fcf8660862f000
Ninz <stninmar>
2017-05-02 12:38:02 +0200

    trigger

--- Programmierung/Datenbank/MySQL/trigger.sql
+++ Programmierung/Datenbank/MySQL/trigger.sql
@@ -25,13 +25,40 @@ create trigger has_teacher_lesson after insert on EventwithTeacher for each row
 	end;//
 delimiter ;
 
-
 Wenn FÜ Stunde eingetragen ist -> ist Prof. der Unterricht haben müsste anwesend oder nicht -> Count -1 oder 0
 
 delimiter //
-create trigger is_teacher_present after insert on Event for each row
-	begin 
-		declare klasse = select SchoolClass from EventwithSchoolClass where Event_ID = New.Event_ID;
-		if 
-		
-		
+create trigger is_teacher_present after insert on EventwithSchoolClass for each row
+	begin
+		declare guthaben int;
+		declare nowHour int;
+		declare hours int;
+		set nowHour = (select Begin_Hour from Event where Event_ID = New.Event_ID);
+		set hours = (select (End_Hour - Begin_Hour + 1) from Event where Event_ID = New.Event_ID);
+		set guthaben = hours * (-1);
+		while hours >= 0 do 
+			if (select Teacher_Name from EventwithTeacher natural join Event as e natural join Timetable as tt where SchoolClass_Description = new.SchoolClass_Description 
+			and tt.SchoolHour = nowHour)
+			then set guthaben = guthaben + 1;
+			end if;
+			set hours = hours - 1;
+			set nowHour = nowHour + 1;
+		end while;
+		update Teacher set Credit = Credit + guthaben where Teacher.Teacher_Name = teacherName;
+	end;//
+delimiter ;
+
+select Teacher_Name from EventwithTeacher natural join Event as e natural join Timetable as tt where SchoolClass_Description = new.SchoolClass_Description 
+	and tt.SchoolHour between e.Begin_Hour and e.End_Hour
+	
+select Teacher_Name from Event natural join EventwithSchoolClass natural join Timetable where SchoolClass_Description = new.SchoolClass_Description
+	and 
+	
+	
+	
+	
+	FÜ			Unterricht
+	abrim		alper
+	abrim		abrim
+	apler		abrim
+	

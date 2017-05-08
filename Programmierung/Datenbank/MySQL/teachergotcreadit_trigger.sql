bd8cd1534d29429037fb84e8850fc558107cd8b8
Ninz <stninmar>
2017-05-08 15:44:46 +0200

    button lehrer hinzufuegen und kleine aenderungen

--- Programmierung/Datenbank/MySQL/teachergotcreadit_trigger.sql
+++ Programmierung/Datenbank/MySQL/teachergotcreadit_trigger.sql
@@ -1,12 +1,12 @@
 DELIMITER $$
 create trigger is_event_deleted before delete on Event for each row
-	BLOCK1 BEGIN
+	BLOCK1: BEGIN
 		DECLARE teacher_finished INTEGER DEFAULT 0;
 		DECLARE teacherName varchar(255);
 		DECLARE credit_Change INTEGER;
 		-- Cursor für alle Lehrer wo sich der Credit durch das Event gändert hat
 		DEClARE teacher_cursor CURSOR FOR
-			select Techer_Name from TeacherGotCreadit where Event_ID = old.Event_ID;
+			select Techer_Name from TeacherGotCredit where Event_ID = old.Event_ID;
 		-- declare NOT FOUND handler
 		DECLARE CONTINUE HANDLER 
 			FOR NOT FOUND SET teacher_finished = 1;
@@ -18,11 +18,11 @@ create trigger is_event_deleted before delete on Event for each row
 					LEAVE get_teacher;
 				END IF;
 				
-				BLOCK2 BEGIN
+				BLOCK2: BEGIN
 					DECLARE teacherCreditChange_finished INTEGER DEFAULT 0;
 					-- Cursor für alle Guthabeneinträge welche eingetragen wurden
 					DEClARE teacherCreditChange_cursor CURSOR FOR
-						select CreditChange from TeacherGotCreadit where Event_ID = old.Event_ID and Teacher_Name = teacherName;
+						select CreditChange from TeacherGotCredit where Event_ID = old.Event_ID and Teacher_Name = teacherName;
 					-- declare NOT FOUND handler
 					DECLARE CONTINUE HANDLER 
 						FOR NOT FOUND SET teacherCreditChange_finished = 1;
@@ -35,10 +35,11 @@ create trigger is_event_deleted before delete on Event for each row
 						END IF;
 						update Teacher set Creadit = Credit - credit_Change where Teacher_Name = teacherName;
 					END LOOP get_CreditChange;
+					CLOSE teacherCreditChange_cursor;
 				END BLOCK2;
 			END LOOP get_teacher;
 		CLOSE teacher_cursor;		
-	BLOCK1 END;$$
+	BLOCK1 END; $$
 DELIMITER ;
 
 

3feae3288629b713ee34e0cc3ea4a460527744e8
Gasteiger <stgastob>
2017-05-16 08:01:29 +0200

    VBA MAkros in EXcel Tabelle hinzugefügt

---
+++ Programmierung/Datenbank/MySQL/inserts/Simulation/Facher.sql
@@ -0,0 +1,58 @@
+INSERT IGNORE INTO Subject (Subject_Description) Values 
+("Angewandte Technologie"),
+("Chemie"),
+("Deutsch"),
+("Englisch"),
+("Elektronik"),
+("Französisch"),
+("Französisch / Russisch"),
+("Geschichte"),
+("Italienisch"),
+("Informatik"),
+("Informationsübertragungsysteme"),
+("Latein"),
+("Latein / Physik"),
+("Mathematik"),
+("Naturkunde"),
+("Physik"),
+("Religion"),
+("Russisch"),
+("Sport"),
+("Systeme und Netze"),
+("Turnen"),
+("Telekommunikation"),
+("Technologie und Planung"),
+("Technisch Zeichnen"),
+("Wirtschaftskunde"),
+("Wirtschaftskunde/ Physik"),
+("Wahrscheinlichkeitsrechnen und Statistik"),
+("Zeichnen und Kunst");
+
+"AT" 	= "Angewandte Technologie"
+"C" 	= "Chemie"
+"D" 	= "Deutsch"
+"E" 	= "Englisch"
+"EL" 	= "Elektronik"
+"F" 	= "Französisch"
+"F/R" 	= "Französisch / Russisch"
+"H" 	= "Geschichte"
+"I" 	= "Italienisch" 
+"INF" 	= "Informatik"
+"IÜ" 	= "Informationsübertragungsysteme"
+"L" 	= "Latein"
+"L/P" 	= "Latein / Physik"
+"M" 	= "Mathematik"
+"N" 	= "Naturkunde"
+"P" 	= "Physik"
+"R" 	= "Religion"
+"RUS" 	= "Russisch"
+"S" 	= "Sport"
+"SN" 	= "Systeme und Netze"
+"T" 	= "Turnen"
+"TK" 	= "Telekommunikation"
+"TP" 	= "Technologie und Planung"
+"TZ" 	= "Technisch Zeichnen"
+"W" 	= "Wirtschaftskunde"
+"W/P"	= "Wirtschaft / Physik"
+"WS" 	= "Wahrscheinlichkeitsrechnen und Statistik"
+"Z"	= "Zeichnen und Kunst"

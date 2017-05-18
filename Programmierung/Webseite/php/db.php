1c91bf17b9d81f73ed918047bceb27f48370b6e4
Rafael Steiner <steiner.rafael97@gmail.com>
2017-05-18 17:44:50 +0200

    Search DB Anbindung

---
+++ Programmierung/Webseite/php/db.php
@@ -0,0 +1,4 @@
+<?php 
+	$con=mysql_connect("localhost","ststeraf","mypass");
+    $db=mysql_select_db("projekt_fu",$con);
+?>
--- Programmierung/Webseite/php/updateKlassen/search.php
+++ Programmierung/Webseite/php/updateKlassen/search.php
@@ -1,8 +1,7 @@
 <?php
+	include("../db.php");
     $key=$_GET['key'];
     $array = array();
-    $con=mysql_connect("linuxserver","ststeraf","mypass");
-    $db=mysql_select_db("projekt_fu",$con);
     $query=mysql_query("select * from SchoolClass where SchoolClass_Description LIKE '{$key}%'");
     while($row=mysql_fetch_assoc($query))
     {

c1a8c92e365245004966eaf0929e2aa0dfba4f36
Rafael <steiner.rafael97@gmail.com>
2017-05-23 08:56:18 +0200

    Webseite - Edit Feld

--- Programmierung/Webseite/php/includes/indexAdmin/indexAdminDynamicTable.php
+++ Programmierung/Webseite/php/includes/indexAdmin/indexAdminDynamicTable.php
@@ -225,8 +225,162 @@
 		createModalforChangeEvent($row);
 	}
 	
-	function createModalforChangeEvent($row){
-		//Edit with NINZ code
+	function createModalforChangeEvent($row){	
+	
+		include("db.php");
+		$lehrer = $db->query("select Teacher_Name from Event natural join EventwithTeacher where Event_ID = $row->Event_ID;");
+		$db->close();
+
+		//Modal beginnen
+		$modal = 
+		"<div id='modalEditEvent$row->Event_ID' class='modal bottom-sheet'>
+			<form action='' method='post'>
+				<div class='modal-content'>
+					<h4>Hinzufügen</h4>
+					<div class='row'>
+						<div class='input-field col s6'>";
+							
+			//TITEL auffüllen
+			if ($row->Titel != null || $row->Titel != ""){
+				$modal .= "<input id='first_name' type='text' name='titel' class='validate' value='$row->Titel' required>";
+			}else{
+				$modal .= "<input id='first_name' type='text' name='titel' class='validate' required>";
+			}
+			
+			$modal .= "	<label for='first_name'>Titel *</label></div>
+						<div class='input-field col s6'>";
+			
+			//Beschreibung auffüllen
+			if ($row->Description != null || $row->Description != ""){
+				$modal .= "<input id='first_name' type='text' name='beschreibung' class='validate' value='$row->Description'>";
+			}else{
+				$modal .= "<input id='first_name' type='text' name='beschreibung' class='validate'>";
+			}
+			
+			$modal .= "	<label for='first_name'>Beschreibung</label></div>
+						<div class='input-field col s6'>";
+						
+			//Datum auffüllen
+			if ($row->Date != null || $row->Date != ""){
+				$modal .= "<input id='first_name' type='text' name='datum' class='validate' value='$row->Date'>";
+			}else{
+				$modal .= "<input id='first_name' type='text' name='datum' class='validate'>";
+			}
+		
+			$modal .= "	<label for='first_name'>Datum *</label></div>
+						<div class='input-field col s6'>";
+						
+			//Zusatzpersonen auffüllen
+			if ($row->Person != null || $row->Person != ""){
+				$modal .= "<input id='first_name' type='text' name='zusatzpersonen' class='validate' value='$row->Person'>";
+			}else{
+				$modal .= "<input id='first_name' type='text' name='zusatzpersonen' class='validate'>";
+			}
+			
+			$modal .= "	<label for='first_name'>Zusatzpersonen</label></div>
+						<div class='input-field col s6'>";
+						
+			//Anfangsstunde auffüllen
+			if ($row->Begin_Hour != null || $row->Begin_Hour != ""){
+				$modal .= "<input id='first_name' type='text' name='anfangsstunde' class='validate' value='$row->Begin_Hour'>";
+			}else{
+				$modal .= "<input id='first_name' type='text' name='anfangsstunde' class='validate'>";
+			}
+			
+			$modal .= "	<label for='first_name'>Anfangsstunde *</label></div>
+						<div class='input-field col s6'>";
+						
+			//EndStunde auffüllen
+			if ($row->End_Hour != null || $row->End_Hour != ""){
+				$modal .= "<input id='first_name' type='text' name='endstunde' class='validate' value='$row->End_Hour'>";
+			}else{
+				$modal .= "<input id='first_name' type='text' name='endstunde' class='validate'>";
+			}
+			
+			$modal .= "	<label for='first_name'>EndStunde *</label></div>
+						<div class='input-field col s6'>";
+			
+			//Lehrer auffüllen
+			if ($row->End_Hour != null || $row->End_Hour != ""){
+				$modal .= "<input type='text' id='lehrer' class='typeahead2 tt-query' autocomplete='off' spellcheck='false'></input>";
+			}else{
+				$modal .= "<input type='text' id='lehrer' class='typeahead2 tt-query' autocomplete='off' spellcheck='false' placeholder='Lehrer suchen'></input>";
+			}		
+		
+			//Lehrer einfügen
+			$modal .= "<p>Lehrer:";
+			while($rowLehrer = $lehrer->fetch_object()) {
+				$modal .= " $rowLehrer->Teacher_Name,";
+			}
+			
+			
+			$modal .= 
+			"			</div>
+					</div>
+				</div>
+					<div class='modal-footer'>	
+						<button type='submit' class='modal-action waves-effect waves-light btn-flat' name='btnChange'>Änderung speichern</button>
+						<input type='submit' class='modal-action waves-effect waves-light btn-flat modal-close' value='Abbrechen'></input>
+					</div>
+				</form>
+			</div>";
+		echo $modal;
+		
+		if(isset($_POST["btnChange"])){
+			global $db;		
+			$klasse = $_SESSION["klasse"];
+			$titel = $_POST["titel"];
+			$beschreibung = $_POST["beschreibung"];
+			$datum = $_POST["datum"];
+			$zusatzpersonen = $_POST["zusatzpersonen"];
+			$anfangsstunde = $_POST["anfangsstunde"];
+			$endstunde = $_POST["endstunde"];
+			
+			$getTeacherOfEvent = $db->query("select * from EventwithTeacher where Event_ID = $row->Event_ID;");
+			$lehrer = array();
+			$i=0;
+			
+			while($zeile = $getTeacherOfEvent->fetch_object()) {
+				$lehrer[$i] =  $zeile->Teacher_Name;
+				$i++;
+			}
+			
+			//Eintrag aus der Datenbank löschen
+			$sqlDelete = $db->query("delete from EventwithSchoolClass where Event_ID = $row->Event_ID");
+			$sqlDelete = $db->query("delete from EventwithTeacher where Event_ID = $row->Event_ID");
+			$sqlDelete = $db->query("delete from Event where Event_ID = $row->Event_ID");
+
+			$sql= $db->prepare("insert into Event(Titel,Description,Date,Person,Begin_Hour,End_Hour) VALUES(?,?,?,?,?,?)");
+			$sql->bind_param("ssssii", $titel, $beschreibung, $datum, $zusatzpersonen, $anfangsstunde, $endstunde);
+			
+			if($sql->execute()){
+				$eventID = $sql->insert_id;
+				
+				$i = 0;
+				while($i < count($lehrer)) {
+					$sql3 = $db->prepare("insert into EventwithTeacher(Event_ID, Teacher_Name) VALUES (?,?);");
+					$sql3->bind_param("is", $eventID, $lehrer[$i]);
+					if($sql3->execute()) {}
+					else {
+						echo "<script type='text/javascript'>alert(Fehler - Daten konnten nicht hinzugefügt werden');</script>";
+					}
+					$sql3->close();
+					$i++;
+				}
+				
+				$sql2 = $db->prepare("insert into EventwithSchoolClass(Event_ID, SchoolClass_Description) VALUES (?,?);");
+				$sql2->bind_param("is", $eventID, $klasse);
+				if($sql2->execute()) {								
+					echo "<script type='text/javascript'>alert('Daten hinzugefügt!');</script>";
+				}else{
+					echo "<script type='text/javascript'>alert(Fehler - Daten konnten nicht hinzugefügt werden');</script>";
+				}
+			}else{
+				echo "<script type='text/javascript'>alert(Fehler - Daten konnten nicht hinzugefügt werden');</script>";
+			}
+		}
 	}
+	
+	
 	
 ?>
